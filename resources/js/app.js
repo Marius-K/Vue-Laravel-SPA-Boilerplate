907fd7e13e4cfa82348adc9bba811619c8e5c6ac
Marius Kasparavičius <marre124@gmail.com>
2020-09-04 17:14:30 +0300

    Installed Vuetify and Material Design Icons
    npm install vuetify
    npm install @mdi/font -D

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,14 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 import routes from './routes'
 
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+
 import App from './components/App.vue'
 
 const app = new Vue({
     el: '#app',
     components: { App },
-    router: new VueRouter(routes)
+    router: new VueRouter(routes),
+    vuetify: new Vuetify()
 })
